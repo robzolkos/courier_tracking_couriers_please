04575c03dffbee30cfe58cf70291e0c8bfc97322
Rob Zolkos <rob@zolkos.com>
2017-10-16 13:12:08 +1000

    Convert dates in UTC

--- helpers/formatDate.js
+++ helpers/formatDate.js
@@ -2,7 +2,7 @@ const moment = require("moment");
 
 function formatDate(date_part, time_part) {
   const date_to_convert = date_part + " " + time_part;
-  const parsed_date = moment(date_to_convert, "D MMM YYYY h:mm a");
+  const parsed_date = moment.utc(date_to_convert, "D MMM YYYY h:mm a");
   const date_string = parsed_date.format("MMM D, YYYY");
   const time_string = parsed_date.format("h:mma");
   const valueDate = parsed_date.valueOf();
--- helpers/formatDate.test.js
+++ helpers/formatDate.test.js
@@ -5,7 +5,7 @@ describe("formatDate", () => {
     expect(formatDate("22 Sep 2017", "11:01 am")).toEqual({
       date: "Sep 22, 2017",
       time: "11:01am",
-      value: 1506042060000
+      value: 1506078060000
     });
   });
 });
